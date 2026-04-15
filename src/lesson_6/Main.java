3ea954ad3637799b3f264936de05590aaf68b4ca
noskovaolena7 <noskovaolena7@gmail.com>
2026-04-15 13:54:38 +0200

    add lesson 6

---
+++ src/lesson_6/Main.java
@@ -0,0 +1,26 @@
+package lesson_6;
+
+public class Main {
+
+    // static variable
+    static double PI = 3.14;
+
+    //class/instance variable
+    int b = 5;
+
+    void main() {
+        // local variable
+        int a = 10;
+
+        // local variable
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(PI);
+
+}
+    void print() {
+        System.out.println(b);
+        System.out.println(PI);
+
+    }
+}
