500bf1995a659eec46d895242056c222e6dfb734
noskovaolena7 <noskovaolena7@gmail.com>
2026-04-28 11:01:22 +0200

    - made correct

--- src/lesson_12_loops/Main.java
+++ src/lesson_12_loops/Main.java
@@ -69,25 +69,34 @@ public class Main {
                 System.out.println(arr[i] + " ");
             }
         }
-
         // Написати програму що робить усі від’ємні елементи масиву
         // додатніми і збільшує їх удвічі застосовуючи елвіс оператор.
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] < 0 ? arr[i] * -2 : arr[i];
-        }
 
         for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] < 0 ? arr[i] * -2 : arr[i];
             System.out.print(arr[i] + " ");
         }
 
+        /*for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        */
         // Створити масив int та ініціалізувати його будь-якими числами
-
+        System.out.println();
         int [] numberCars = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
             // Вивести до консолі лише парні числа масиву
             for (int k = 0; k <= 10; k = k+2) {
 
-                System.out.println(k);
+                System.out.println(k + " ");
+            }
+
+        System.out.println();
+            int [] array = {9, 11, 2, 14, 7, 17, 20, 32};
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 == 0) {
+                    System.out.println(array[i]);
+                }
             }
 
             }
